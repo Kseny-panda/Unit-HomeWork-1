d92cbd34715616274e9ca2332457fa2f186e8710
Kseniya <i.pretzel16@gmail.com>
2025-03-18 14:49:40 +0300

    Add Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,46 +1,44 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-
-import static org.testng.Assert.*;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
     // Нет товаров в корзине
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldWithoutPurchases() {
 
-        Assert.assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(1000, service.remain(0));
     }
 
     // Минимальная покупка
-    @org.testng.annotations.Test
-    public void shouldMinPurchases() {
+    @org.junit.Test
+    public void shouldMinimumPurchase() {
 
-        Assert.assertEquals(service.remain(1), 999);
+        Assert.assertEquals(999, service.remain(1));
     }
 
     // Покупка в пределах границ
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldPurchaseWithinBorders() {
 
-        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(1, service.remain(999));
     }
 
     // Покупка по верхней минимальной границе для начисления кешбека
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldPurchaseUpperLimit() {
 
-        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(0, service.remain(1000));
     }
 
     // Покупка выше верхней границы минимальной покупки для начисления кешбека
     // для нового расчета программа предлагает добавить товар.
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAboveUpperLimit() {
 
-        Assert.assertEquals(service.remain(1001), 999);
+        Assert.assertEquals(999, service.remain(1001));
     }
 }
